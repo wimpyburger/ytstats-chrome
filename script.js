71fa0a120931c0709775ee25b8af0dcf75e3a018
wimpyburger <xump@null.net>
2015-08-31 20:22:16 +0100

    Changed script to use buffer

--- script.js
+++ script.js
@@ -17,6 +17,8 @@
 // @version       0.1
 // ==/UserScript==
 
+var videobuffer = [];
+var submitinterval;
 var pageloadedinterval;
 var regularinterval;
 var siteurl = "http://www.turnaroundtoolkit.co.uk/inc/submit.php";
@@ -24,24 +26,35 @@ var xmlhttp = new XMLHttpRequest();
 var c_username = "changeme";
 var c_password = "changeme";
 
-function sendvideo(videourl) {
+function addtobuffer(videourl) {
     var videoid = videourl.substr(videourl.lastIndexOf('v=') + 2);
-    //var tab = siteurl + "?username=" + encodeURIComponent(username) + "&password=" + encodeURIComponent(password) + "&id=" + videourl;
+    videobuffer.push(videoid);
+}
+
+function vidsubmit(index) {
     GM_xmlhttpRequest({
-        method: "POST",
-        url: siteurl,
-        data: "username=" + c_username + "&password=" + c_password + "&id=" + videoid,
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        onload: function(response) {
-			console.log(response.responseText);
-            if(response.responseText == "DONE") {
-                console.log("Video sent to Ytstats");
-                clearInterval(regularinterval);
+            method: "POST",
+            url: siteurl,
+            data: "username=" + c_username + "&password=" + c_password + "&id=" + videobuffer[index],
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            onload: function(response) {
+                console.log(response.responseText);
+                if(response.responseText == "DONE") {
+                    console.log("Video sent to Ytstats");
+                    videobuffer.splice(index, 1);
+                }
             }
-        }
     });
+}
+
+function buffersubmit() {
+    for (var index = 0; index < videobuffer.length; index++) {
+        console.log(videobuffer);
+        // submit
+        vidsubmit(index);
+    }
 }
 
 function checkplaytime() {
@@ -50,7 +63,9 @@ function checkplaytime() {
         var halflength = ytplayer.getDuration() / 2;
         var currenttime = ytplayer.getCurrentTime();
         if(currenttime >= halflength) {
-            sendvideo(ytplayer.getVideoUrl());
+            clearInterval(regularinterval);
+            addtobuffer(ytplayer.getVideoUrl());
+            //sendvideo(ytplayer.getVideoUrl());
         }
     }
 }
@@ -66,6 +81,7 @@ function reset() {
     clearInterval(regularinterval);
     clearInterval(pageloadedinterval);
     pageloadedinterval = setInterval(checkifloaded, 500);
+    submitinterval = setInterval(buffersubmit, 2000);
 }
 
 function youtubePageChange()
